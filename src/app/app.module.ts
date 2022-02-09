440b750cc2232525e0b78ad0690e321ef655f0b6
kyle <kyle@melissadata.com>
2022-02-09 13:01:21 -0800

    Added title view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/title/title.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  template: `
+    <section parallax-background parallax-ratio="0.2" id="intro" class="title">
+      <a class="logo-link">
+        <span class="logo"></span>
+      </a>
+      <div class="text">
+        <h2 class="title-h2">Hello, I'm</h2>
+        <h1 class="title-h1">&lt;Kyle Houston <span class='slash'>/</span>&gt;</h1>
+        <div>
+          <!-- <img class="title-logo" src="../../inspiration/Logo/logo.png"/> -->
+        </div>
+        <h2 class="title-h2">Front end developer</h2>
+      </div>
+    </section>`,
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
