1d74ac89a4dd2c6ffee1c0591c0fad6621e59cff
kyle <kyle@melissadata.com>
2022-02-10 15:12:43 -0800

    Moved logo on the title view

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -4,16 +4,17 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-title',
   template: `
     <section id="intro" class="title">
-      <a class="logo-link">
+      <!-- <a class="logo-link">
         <span class="logo"></span>
-      </a>
+      </a> -->
       <div class="text">
-        <h2 class="title-h2">Hello, I'm</h2>
+        <div class="logo"></div>
+        <!-- <h2 class="title-h2">Hello, I'm</h2> -->
         <h1 class="title-h1">&lt;Kyle Houston <span class='slash'>/</span>&gt;</h1>
         <div>
           <!-- <img class="title-logo" src="../../inspiration/Logo/logo.png"/> -->
         </div>
-        <h2 class="title-h2">Front end developer</h2>
+        <h2 class="title-h2">web application developer</h2>
       </div>
     </section>`,
   styleUrls: ['./title.component.scss']
