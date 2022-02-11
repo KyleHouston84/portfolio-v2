ad5cd5e7301519bec872c308a12589494d82beef
kyle <kyle@melissadata.com>
2022-02-11 00:49:33 -0800

    Added video to contact section

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -7,11 +7,12 @@ import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
   template: `
   <section id="contact" class="contact sm">
     <!-- <video-load video-source="/assets/video/contact_vid.mp4"></video-load> -->
+    <video id="video-bg" width="100%" loop autoplay preload="auto" class="contact-bg"><source id="video-src" src="/assets/video/contact_vid.mp4" type="video/mp4"></video>
     <div class="foreground">
       <h2 class="section-title">&lt;Let's Talk! <span class="slash">/</span>&gt;</h2>
       <div class="contact-me">
         <p class="contact-text">
-          Interested in working with me? Say hi, I'll respond ASAP:
+          Interested in working with me? Say hi, I'll respond ASAP
         </p>
         <span class="links">
           <a href="mailto:kyle.houston84@gmail.com" class="email">                              <fa-icon [icon]="faEnvelope" size="2x"></fa-icon></a>
