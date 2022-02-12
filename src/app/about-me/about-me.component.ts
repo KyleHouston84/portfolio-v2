3e32493ae3d315d3a391e41b016a41a4516b201e
kyle <kyle@melissadata.com>
2022-02-12 02:36:42 -0800

    Updated resume link

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -20,11 +20,13 @@ import { Component, OnInit } from '@angular/core';
           </p>
 
           <p  class="mobile-optional">
+            I am passionate about delivering high-quality software throughout the development cycle with the ability to quickly learn a 
+            broad range of skills/technologies to meet requirements.
             I excel in transforming a design into living web components and interactions. I fill the gaps between
             the breakpoints and design additional components following the look and feel of the original design.
             I love working collaboratively with designers and other developers to make engaging and beautiful web applications.
           </p>
-          <a class="button" href="https://drive.google.com/open?id=0ByNa4iGTcKzqWThTY0dZaU1xTFE" target="_blank">Download my resume</a>
+          <a class="button" href="https://drive.google.com/file/d/1pGdFWpIivMHPHubBvdYN_4kf4Uv3x6Z8/view?usp=sharing" target="_blank">Download my resume</a>
         </div>
       </article>
     </section>`,
